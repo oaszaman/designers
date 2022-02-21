ae5138224df5f2e75cc2e692d4a059a9583a1429
Shahzab A Zaman <o.aszaman00@gmail.com>
2022-02-20 20:08:44 -0600

    cli greeting

--- lib/designers.rb
+++ lib/designers.rb
@@ -2,6 +2,20 @@
 
 require_relative "designers/version"
 
+require_relative "designers/cli"
+require_relative "designers/designerdescription"
+require_relative "designers/scraper"
+require_relative "designers/upcomingdesigners"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+require 'colorized_string'
+#require 'watir'
+#require 'webdrivers'
+
+
 module Designers
   class Error < StandardError; end
   # Your code goes here...
---
+++ lib/designers/cli.rb
@@ -0,0 +1,10 @@
+class Designers::CLI
+    @@blue="\e[3;34m"
+
+    def greeting
+        puts "\n#{@@blue}Welcome! If you’re looking to know about 11 Up-And-Coming Designers for 2022, \nthen you’re in the right place. To continue press Y.#{@@blue}"
+        puts "\n"
+
+    end
+
+end
